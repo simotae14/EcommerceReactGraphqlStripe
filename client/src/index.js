6d08a435db36ea7fdff307196d55ba6194091a8b
simotae14 <startae14@gmail.com>
2019-05-29 23:41:44 +0200

    add some new routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+const Root = () => {
+    <Router>
+        <Switch>
+            <Route
+
+            />
+        </Switch>
+    </Router>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
