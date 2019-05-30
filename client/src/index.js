99a182481ed3bfa78cf5790fc30a44cafb80859a
simotae14 <startae14@gmail.com>
2019-05-30 23:29:17 +0200

    define the routing and the routes

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,35 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Checkout from './components/Checkout';
+
 import registerServiceWorker from './registerServiceWorker';
 
-const Root = () => {
+const Root = () => (
     <Router>
         <Switch>
             <Route
-
+                exact
+                component={App}
+                path="/"
+            />
+            <Route
+                component={Signin}
+                path="/signin"
+            />
+            <Route
+                component={Signup}
+                path="/signup"
+            />
+            <Route
+                component={Checkout}
+                path="/checkout"
             />
         </Switch>
     </Router>
-}
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
