5bb43b00be3ac96ba389aeaf42105fc45f1748e8
Simone Taeggi <startae14@gmail.com>
2019-06-11 00:26:40 +0200

    Add Heading for the App Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { Container, Box, Heading } from 'gestalt';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Ciao
-      </div>
+      <Container>
+        {/* Brands Section */}
+        <Box
+          display="flex"
+          justifyContent="center"
+          marginBottom={2}
+        >
+          {/* Brands Header */}
+          <Heading
+            color="midnight"
+            size="md"
+          >
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
     );
   }
 }
