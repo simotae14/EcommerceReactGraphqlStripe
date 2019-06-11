ac616d6582a9631a1fed37bdfb97dc12e49ccf21
Simone Taeggi <startae14@gmail.com>
2019-06-11 18:19:39 +0200

    set the local State of App with the brands response

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,24 +6,31 @@ const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
 
 class App extends Component {
+  state = {
+    brands: []
+  }
   async componentDidMount() {
-    const response = await strapi.request('POST', '/graphql', {
-      data: {
-        query: `query{
-          brands {
-            _id
-            name
-            description
-            createdAt
-            image {
+    try {
+      const response = await strapi.request('POST', '/graphql', {
+        data: {
+          query: `query{
+            brands {
+              _id
               name
-              url
+              description
+              image {
+                url
+              }
             }
-          }
-        }`
-      }
-    });
-    console.log(response);
+          }`
+        }
+      });
+      this.setState({
+        brands: response.data.brands
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
