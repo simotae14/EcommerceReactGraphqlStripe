4bc6273d1a78c3bcf3c944dc3473913d4eb5f285
Simone Taeggi <startae14@gmail.com>
2019-06-11 18:38:19 +0200

    Display brands as cards using gestalt

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Box, Heading } from 'gestalt';
+import { Container, Box, Heading, Card, Image, Text } from 'gestalt';
+import { Link } from 'react-router-dom';
 import './App.css';
 import Strapi from 'strapi-sdk-javascript/build/main';
 const apiUrl = process.env.API_URL || 'http://localhost:1337';
@@ -34,6 +35,7 @@ class App extends Component {
   }
 
   render() {
+    const { brands } = this.state;
     return (
       <Container>
         {/* Brands Section */}
@@ -49,6 +51,63 @@ class App extends Component {
           >
             Brew Brands
           </Heading>
+        </Box>
+        {/* Brands */}
+        <Box
+          display="flex"
+          justifyContent="around"
+        >
+          {
+            brands.map( brand => (
+              <Box
+                margin={2}
+                width={200}
+                key={brand.id}
+              >
+                <Card
+                  image={
+                    <Box
+                      height={200}
+                      width={200}
+                    >
+                      <Image
+                        alt="Brand"
+                        naturalHeight={1}
+                        naturalWidth={1}
+                        src={`${apiUrl}${brand.image.url}`}
+                      />
+                    </Box>
+                  }
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    direction="column"
+                  >
+                    <Text
+                      size="xl"
+                    >
+                      {brand.name}
+                    </Text>
+                    <Text>
+                      {brand.description}
+                    </Text>
+                    <Text
+                      size="xl"
+                    >
+                      <Link
+                        to={`${brand._id}`}
+                      >
+                        See Brews
+                      </Link>
+                    </Text>
+                  </Box>
+
+                </Card>
+              </Box>
+            ))
+          }
         </Box>
       </Container>
     );
